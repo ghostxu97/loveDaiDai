8e4041c95802b49e5497caa561d9bfa787e68b2b
GhostXu <43178911+ghostxu97@users.noreply.github.com>
2022-08-23 16:30:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def get_weather():
   url = "https://v0.yiketianqi.com/api?unescape=1&version=v91&appid=43656176&appsecret=I42og6Lm&ext=&cityid=&city=" + city
   res = requests.get(url).json()
   weather = res['data'][0]
-  return weather['wea'], math.floor(weather['temp'])
+  return weather['wea'], math.floor(weather['tem1'], math.floor(weather['tem2'])
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
@@ -47,8 +47,8 @@ def get_random_color():
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
-wea, temperature = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+wea, temperature_min, temperature_max = get_weather()
+data = {"weather":{"value":wea},"temperature":{"value":temperature_min + "~" + temperature_max},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
 res = wm.send_template("ozt7455T6o7SIKhm2h49agvaQtbw", template_id, data)
